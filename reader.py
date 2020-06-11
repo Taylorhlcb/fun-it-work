a237d376fa6c8851d56c321b3219d95ce47a4e9a
unknown <taylortextalert@gmail.com>
2020-06-11 00:16:36 -0700

    'yes'

--- reader.py
+++ reader.py
@@ -2,7 +2,7 @@ from pdfrw import PdfReader
 pdf = PdfReader('setup.pdf')
 print(pdf.Root.AcroForm.Fields)
 print(len(pdf.Root.AcroForm.Fields))
-for a in range(0,16):
+for a in range(0,len(pdf.Root.AcroForm.Fields)):
     print(pdf.Root.AcroForm.Fields[a].T)
 print(pdf.Root.AcroForm.Fields[1].T)
 print('PDF has {} pages'.format(len(pdf.pages)))
